93a7007f1c12cf26f633dca1cca8cca1c6e144cb
febrianas18 <febrianass182@gmail.com>
2024-12-14 22:08:45 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tubes_2_uas_kelompok5
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tubes_2_uas_kelompok5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tubes_2_uas_kelompok5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
