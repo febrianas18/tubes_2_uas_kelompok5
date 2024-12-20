e28d763df8daec0dd55911125b50bff61fb5c4c4
fazya <fazyatiartamevia@gmail.com>
2024-12-20 11:58:50 +0700

    feat : layouting global_component (searchview) ( coding )

---
+++ lib/core/global_component/searchview.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SearchViewWidget extends StatelessWidget {
+  final Function(String) onSearch;
+  final String hintText;
+  const SearchViewWidget(
+      {super.key, required this.onSearch, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+      child: TextField(
+        onChanged: onSearch,
+        decoration: InputDecoration(
+          prefix: const Icon(Icons.search),
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+}
