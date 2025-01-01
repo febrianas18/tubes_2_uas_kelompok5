c637cf1c0fc9e9e482df9009256833f59de41075
franklynrama <frenklynrama@gmail.com>
2025-01-01 19:31:31 +0700

    feat :  setup route utils from feature screen ( coding )

---
+++ lib/core/utils/route_utils.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:get/get.dart';
+
+class RouteApp {
+  static String initial = '/';
+
+  static List<GetPage> routes = [
+    GetPage(
+      name: initial, 
+      page: () => const MainMenuScreen(),
+      )
+  ];
+}
